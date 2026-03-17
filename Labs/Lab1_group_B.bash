a9fe9e26cc410df2f477a8a5a788083370ff4c74
Andrej Gruevski <andrejgruevski1@gmail.com>
2026-03-17 21:47:15 +0100

    Bonus exercise Lab 1 Group B

--- Labs/Lab1_group_B.bash
+++ Labs/Lab1_group_B.bash
@@ -35,10 +35,9 @@ grep '^22' OS2.txt | awk '$4 <=50 {print $1,$2,$4}'
 # 10.
 awk 'NR>1{counter[$1]++} END{for(i in counter) printf "%-20s %s\n", i, counter[i]}' | sort -k2,2nr
 
+
 ## BONUS
-
 # 11.
-
 awk 'NR>1{counter[$1]++; total++}
 END{
 	for(i in counter){
@@ -46,3 +45,20 @@ END{
 	printf "%-20s %5d %6.2f%%\n", i, counter[i],percent
 }
 }' file.txt | sort -k2,2nr
+
+# 12.
+# Даден е CSV фајл кој содржи 8 колони IME, KATEGORIJA, BREND, CENA, VALUTA, BOJA, GOLEMINA и DOSTAPNOST. Да се напише команда која ќе ги конвертира сите цени во USD така што ако валутата е DEN цената да се подели со 56, а потоа за сите производи со цена поголема или еднаква на 500 USD да се примени попуст од 10% и сите вредности за валута да се постават на USD, при што резултатот треба да се испечати во ист CSV формат без промена на редоследот на колоните.
+awk -f',' 'BEGIN{OFS=","}
+NR==1{print; next}
+{
+if($5=="DEN"){
+	$4 = $4 / 56
+}
+if($4 >= 500){
+	$4 = $4 * 0.9
+}
+
+$5="USD"
+
+print
+}' file.csv
