fa14534adaa6582f49e01a56ebbfd09a80836a0b
Andrej Gruevski <andrejgruevski1@gmail.com>
2026-05-16 00:18:59 +0200

    Testing semaphore

--- Threads/src/TestThreads/Main.java
+++ Threads/src/TestThreads/Main.java
@@ -2,14 +2,28 @@ package TestThreads;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 
 class MyThread extends Thread {
     static int counter=0;
+    static Semaphore semaphore = new Semaphore(1);
+
     @Override
     public void run() {
-        for (int i=0;i<20000;i++) {
-            counter++;
+        // pred da vlezime vo kritichniot region barame dozvola
+        // otkako ke izlezime od kritichniot region vrakjame dozvola
+
+        try {
+            semaphore.acquire(); // barame dozvola
+            //kritichen region
+            for (int i=0;i<20000;i++) {
+                counter++;
+            }
+            semaphore.release(); // vrakjame dozvola
+        }catch (InterruptedException e){
+            e.printStackTrace();
         }
+
     }
 }
 public class Main {
